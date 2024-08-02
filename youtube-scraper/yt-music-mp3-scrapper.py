5a28c175b11aedda0b8b8be28f0e0ee26e19b5e6
Ayush-Mgr <yush_mgr@gmail.com>
2024-08-02 17:11:45 +0530

    fg

--- youtube-scraper/yt-music-mp3-scrapper.py
+++ youtube-scraper/yt-music-mp3-scrapper.py
@@ -1,11 +1,10 @@
-from pytube import YouTube
-YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-yt.streamsfilter(progressive=True, file_extension='mp4')
-try:
-    yt.download()
-except:
-    print(f"failed {yt.title}")
-print(f"done {yt.title}")
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
+url = "https://www.youtube.com/watch?v=QP17MZp7yTg"
 
+yt = YouTube(url, on_progress_callback = on_progress)
+print(yt.title)
+
+ys = yt.streams.get_audio_only()
+ys.download(output_path="C:\\Users\\user\\Desktop\\tralpytube",mp3= True )
