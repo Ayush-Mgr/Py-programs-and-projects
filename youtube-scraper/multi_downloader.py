b9dfafcde33dba480d67282c5d33c5ad91a047d8
Ayush-Mgr <yush_mgr@gmail.com>
2024-08-06 18:08:42 +0530

    scrap done

--- youtube-scraper/multi_downloader.py
+++ youtube-scraper/multi_downloader.py
@@ -1,9 +1,19 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-list_url = ["https://www.youtube.com/watch?v=QP17MZp7yTg","https://www.youtube.com/watch?v=-wwEaTae1zw&pp=ygUGbXVzaWNz","https://www.youtube.com/watch?v=cl0a3i2wFcc&pp=ygUGbXVzaWNz","https://www.youtube.com/watch?v=AX6OrbgS8lI&pp=ygUGbXVzaWNz"]
+list_url = [ "https://www.youtube.com/watch?v=DmH6YPWhaDY&pp=ygUFbWVtZXM%3D",
+    "https://www.youtube.com/watch?v=YkE3WuU1DT8&pp=ygULbWVtZXMgc2lnbWE%3D",
+    "https://www.youtube.com/watch?v=RLJ7NnJ7Rpo&pp=ygULbWVtZXMgc2lnbWE%3D"]
+
 for url in list_url:
-  yt = YouTube(url, on_progress_callback = on_progress)
-  print(yt.title)
-  ys = yt.streams.get_audio_only()
-  ys.download(output_path="/content/Downloads",mp3= True )
+  try:
+    yt = YouTube(url, on_progress_callback = on_progress)
+    ffilename = yt.title
+    print(yt.title)
+    ys = yt.streams.get_audio_only()
+    ys.download(filename=ffilename,output_path="/content/Downloads",mp3= True )
+  except Exception as e :
+    print(f"failed : {ffilename},error: {e}")
+    continue
+
+
--- youtube-scraper/yt-music-mp3-scrapper.py
+++ youtube-scraper/yt-music-mp3-scrapper.py
@@ -7,4 +7,4 @@ yt = YouTube(url, on_progress_callback = on_progress)
 print(yt.title)
 
 ys = yt.streams.get_audio_only()
-ys.download(output_path="C:\\Users\\user\\Desktop\\tralpytube",mp3= True )
+new_var = ys.download(,mp3= True )
